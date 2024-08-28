569a28d39bfce4b0fabb92dc54550b5be082110e
Peace Oyewole <oyewolepeace94@gmail.com>
2024-08-28 20:29:41 +0100

    Created Product list app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './Normalize.css';
+import Products from './components/Products.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Products />
+    </div>
+  );
+}
+
+export default App;
