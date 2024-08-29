16b84d2e927488aad8dedc3d8f16051d4714866b
Peace Oyewole <oyewolepeace94@gmail.com>
2024-08-29 17:29:20 +0100

    Used data.json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
 import './Normalize.css';
-import Products from './components/Products.js'
+import Products from './components/Products.js';
 
 function App() {
+
+  // const [cart, setCart] = useState([])
+
+  // const addToCart = (product) => {
+  //   setCart((prevCart) => [...prevCart, product]);
+  // }
   return (
     <div className="App">
       <Products />
+      {/* <Cart cart={cart} /> */}
     </div>
   );
 }
